cf9023d4a94feec182d2f0a3111c8c085a5151b5
Paul Colin Hennig <8d_fvfmaa-o2wf_79aqig6g2ki6-09ffkeqmyo3d@firstdorsal.eu>
2021-12-16 23:57:24 +0100

    change admin pear policy to be everything true

--- src/pearPolicies/pektinAdmin.ts
+++ src/pearPolicies/pektinAdmin.ts
@@ -1,10 +1,10 @@
 export const pektinAdminPearPolicy = `
 package pear_policy
 
-default domain = false
-default api_methods = false
-default rr_types = false
-default value = false
-default ip = false
-default utc_millis = false
+default domain = true
+default api_methods = true
+default rr_types = true
+default value = true
+default ip = true
+default utc_millis = true
 `;
