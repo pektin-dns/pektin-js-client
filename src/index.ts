782daf70cec988d424ee956bbf8c9ef82d59f6ec
Paul Colin Hennig <8d_fvfmaa-o2wf_79aqig6g2ki6-09ffkeqmyo3d@y.gy>
2022-01-30 16:56:48 +0100

    add better absolute name methods

--- src/index.ts
+++ src/index.ts
@@ -418,13 +418,22 @@ export const absoluteName = (name: string) => {
             "Input was undefined. This error indicates an upstream undefined value. Check if all the keys have the right names or use TS."
         );
     }
-    if (!name?.length) return "";
-    name = name.replaceAll(/\s+/g, "");
-    if (name[name.length - 1] !== ".") return name + ".";
-    return name;
+    if (name.endsWith(".")) {
+        return name;
+    } else {
+        return name + ".";
+    }
 };
 
-export const isAbsolute = (name: string): boolean => name[name.length - 1] === ".";
+export const isAbsolute = (name: string): boolean => name.endsWith(".");
+
+export const deAbsolute = (name: string) => {
+    if (name.endsWith(".")) {
+        return name.substring(0, name.length - 1);
+    } else {
+        return name;
+    }
+};
 
 // get the pektin config
 export const getPektinConfig = async (vaultEndpoint: string, vaultToken: string) => {
@@ -550,11 +559,6 @@ ${JSON.stringify(body, null, "    ")}${colors.reset}`
     }
     if (throwErrors) return json as ApiResponseBody;
     return json as ApiResponseBody;
-};
-
-export const deAbsolute = (domainName: string) => {
-    const absDomain = absoluteName(domainName);
-    return absDomain.substring(0, absDomain.length - 1);
 };
 
 export const checkConfidantPassword = (
