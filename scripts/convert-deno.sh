a34becd8d2a69f27ae0b6a34976f323c18729c29
Paul Colin Hennig <8d_fvfmaa-o2wf_79aqig6g2ki6-09ffkeqmyo3d@y.gy>
2022-02-11 14:55:30 +0100

    replace cross fetch with fetch for deno

---
+++ scripts/convert-deno.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+DENO_DIR=dist/deno/
+
+
+mkdir -p ${DENO_DIR}
+cp -r dist/js/* ${DENO_DIR}
+find ${DENO_DIR} -type f -exec sed -i "s/import f from \"cross-fetch\"/const f=fetch/g" {} \;
+
